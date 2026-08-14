8123fd22b8a6e1f317ad28d0d3cdf597c485e4b8
Dipo-Yoga <jacop.angeli00@gmail.com>
2026-08-14 08:19:17 +0200

    feat(Confluence): exploration branch to prove the diamond property from local hypotheses

--- LeanTra.lean
+++ LeanTra.lean
@@ -9,6 +9,7 @@ import LeanTra.Metatheory.Evaluation
 import LeanTra.Metatheory.Reduction
 import LeanTra.Metatheory.Confluence.Diamond
 import LeanTra.Metatheory.Confluence.Orthogonal
+import LeanTra.Metatheory.Confluence.Local
 import LeanTra.Instances.FirstOrder.Terms
 import LeanTra.Instances.FirstOrder.Relations
 import LeanTra.Instances.FirstOrder.SRA
