7641255846dba150dbbf3272542de73b3db5fb06
Dipo-Yoga <jacop.angeli00@gmail.com>
2026-08-31 17:47:07 +0200

    wip(SecondOrder/Lambda): trying nominal sets route

--- LeanTra.lean
+++ LeanTra.lean
@@ -1,12 +1,13 @@
 import LeanTra.Algebra.InvolutiveQuantale
 import LeanTra.Algebra.KleeneStar
+import LeanTra.Algebra.Diamond
 import LeanTra.SRA.Basic
 import LeanTra.SRA.Howe
 import LeanTra.SRA.Modality
 import LeanTra.SRA.OperationalDecomposition
+import LeanTra.Nominal.Basic
 import LeanTra.Metatheory.GentzenPrinciples
 import LeanTra.Metatheory.Evaluation
-import LeanTra.Algebra.Diamond
 import LeanTra.Metatheory.Confluence.ParallelReduction
 import LeanTra.Metatheory.Confluence.Orthogonal
 import LeanTra.Metatheory.Confluence.Local
